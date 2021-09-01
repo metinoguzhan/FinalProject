45f93656e09008884d7f2dd764a664ab60ba4bfc
Metin Oguzhan <isslandbul@hotmail.com>
2021-09-01 12:25:46 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kondları
+            //Yetkisi var mı ? 
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            productManager.GetAll().ForEach(x=>Console.WriteLine("{0} - {1}",x.ProductId,x.ProductName));
+            
+        }
+    }
+}
