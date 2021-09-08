caa8f394efb12e6cdf21613fa77d0ccfa6e940f9
Metin Oguzhan <isslandbul@hotmail.com>
2021-09-08 22:54:38 +0300

    Engin Demirog

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategoryDal _category;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _category = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _category.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _category.Get(p => p.CategoryId == categoryId);
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,20 +10,46 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             //Console UI
-            ProductManager productManager = new ProductManager(new EfProductDal());
-            
-            Console.WriteLine("**   Bütün  Liste   **");
-            productManager.GetAll().ForEach(x=>Console.WriteLine("{0} - {1}",x.ProductId,x.ProductName,x.UnitPrice));
+            ForProduct();
+            ForCategory();
+        }
+
+        private static void ForCategory()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            Console.WriteLine("**   Kategori bütün  Liste   **");
+            categoryManager.GetAll().ForEach(p => Console.WriteLine("{0} - {1}", p.CategoryId, p.CategoryName));
             Console.WriteLine("-------------------------------------------------------\n");
-            
+
+
+            Console.WriteLine("**   Kategori 1 Nolu  Liste   **");
+            Console.WriteLine("{0} - {1}", categoryManager.GetById(1).CategoryId, categoryManager.GetById(1).CategoryName);
+            Console.WriteLine("-------------------------------------------------------\n");
+
+
+            Console.WriteLine("**   Kategori 2 Nolu  Liste   **");
+            Console.WriteLine("{0} - {1}", categoryManager.GetById(2).CategoryId, categoryManager.GetById(2).CategoryName);
+            Console.WriteLine("-------------------------------------------------------\n");
+        }
+
+        private static void ForProduct()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            Console.WriteLine("**   Bütün  Liste   **");
+            productManager.GetAll().ForEach(x => Console.WriteLine("{0} - {1}", x.ProductId, x.ProductName, x.UnitPrice));
+            Console.WriteLine("-------------------------------------------------------\n");
+
             Console.WriteLine("**   KategoriId Göre  **");
-            productManager.GetAllByCategoryId(1).ForEach(p => Console.WriteLine("{0} - {1} - {2}", p.ProductId, p.ProductName,p.UnitPrice));
+            productManager.GetAllByCategoryId(1)
+                .ForEach(p => Console.WriteLine("{0} - {1} - {2}", p.ProductId, p.ProductName, p.UnitPrice));
             Console.WriteLine("-------------------------------------------------------\n");
 
             Console.WriteLine("**   Fiyata Göre  **");
-            productManager.GetByUnitPrice(10,15).ForEach(p=> Console.WriteLine("{0} - {1} - {2}", p.ProductId, p.ProductName, p.UnitPrice));
+            productManager.GetByUnitPrice(10, 15)
+                .ForEach(p => Console.WriteLine("{0} - {1} - {2}", p.ProductId, p.ProductName, p.UnitPrice));
             Console.WriteLine("-------------------------------------------------------\n");
-
+            Console.WriteLine("\n\n");
         }
     }
 }
